24ec086de93a04599ae803e9a4433cba408560f5
Matheus Palinkas <matheuspalinkas31@gmail.com>
2020-03-21 19:43:30 -0300

    save posts
    Rotas e pagina de criação do post

--- src/main/java/com/Spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/Spring/codeblog/controller/CodeblogController.java
@@ -3,12 +3,18 @@ package com.Spring.codeblog.controller;
 import com.Spring.codeblog.model.Post;
 import com.Spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -32,5 +38,21 @@ public class CodeblogController {
 
         mv.addObject("post", post);
         return mv;
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+    public String getPostForm(){
+        return  "newPost";
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors())
+            return "redirect:/newpost";
+
+        post.setData(LocalDate.now());
+        codeblogService.save(post);
+
+        return  "redirect:/posts";
     }
 }
