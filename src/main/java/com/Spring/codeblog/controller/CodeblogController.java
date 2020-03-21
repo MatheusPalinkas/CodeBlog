e12d5eaf15c2eb47a62348faa5749db83ab2fd88
Matheus Palinkas <matheuspalinkas31@gmail.com>
2020-03-21 10:28:24 -0300

    Detalhes do post
    Rota e pagina de detalhe dos post

--- src/main/java/com/Spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/Spring/codeblog/controller/CodeblogController.java
@@ -4,6 +4,7 @@ import com.Spring.codeblog.model.Post;
 import com.Spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,6 +22,15 @@ public class CodeblogController {
         List<Post> posts = codeblogService.findeAll();
 
         mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id){
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = codeblogService.findById(id);
+
+        mv.addObject("post", post);
         return mv;
     }
 }
